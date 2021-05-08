244165706e1d92d97fd6cef58a6e822d4c5ef584
sernik <nickstagman@gmail.com>
2021-05-08 15:33:12 -0400

    updates

--- linked_matrix.py
+++ linked_matrix.py
@@ -1,28 +1,9 @@
 from __future__ import annotations
+from typing import Generator, List
 
 
-@staticmethod
-def cover(node: Node) -> None:
-    col = node.get_col()
-    col.right.left = col.left
-    col.left.right = col.right
-    for d in col.itr_down(excl=True):
-        for r in d.itr_right(excl=True):
-            r.up.down = r.down
-            r.down.up = r.up
-            r.get_col().count -= 1
-
-@staticmethod
-def uncover(node: Node) -> None:
-    col = node.get_col()
-    for u in col.itr_up(excl=True):
-        for l in u.itr_left(excl=True):
-            l.up.down = l
-            l.down.up = l
-            l.get_col().count += 1
-    col.right.left = col
-    col.left.right = col
-
+uln = '\033[4m'
+res = '\033[0m'
 
 # Toroidally Linked Matrix
 class DL_Matrix:
@@ -31,7 +12,7 @@ class DL_Matrix:
         self.rows: list[Node] = [Node(self, row=i, col=-1) for i in range(num_rows)]
         self.cols: list[Node] = [Node(self, row=-1, col=i, count=0) for i in range(num_cols)]
         self.solved: bool = False
-
+        # set up links for row and column headers
         for i, node in enumerate(self.rows):
             node.right = node
             node.left = node
@@ -42,16 +23,13 @@ class DL_Matrix:
             node.down = node
             node.right = self.cols[i+1] if i < len(self.cols)-1 else self.root
             node.left = self.cols[i-1] if i > 0 else self.root
+        # set root links to row and column headers
         self.root.right = self.cols[0]
         self.root.left = self.cols[-1]
         self.root.down = self.rows[0]
         self.root.up = self.rows[-1]
 
-    # Returns True if matrix is empty, False Otherwise
-    def is_empty(self):
-        return self.root.right == self.root
-
-    # Returns the header of column with minimum number of 1's
+    # returns the column header of column with minimum number of 1's
     def select_min_col(self) -> Node:
         if self.is_empty(): return self.root
         min_node = self.root.right
@@ -62,38 +40,115 @@ class DL_Matrix:
                 min_count = col.count
         return min_node
 
-    # Inserts Node into Matrix at row, col
-    # If node already exists at row, col: do nothing
+    # cover a column of a node for dancing links algorithm x
+    @staticmethod
+    def cover(node: Node) -> None:
+        col = node.get_col()
+        # unlink left and right neighbors of column header
+        col.right.left = col.left
+        col.left.right = col.right
+        # iterate through each node in column top to bottom
+        for down_itr in col.itr_down(excl=True):
+            # iterate though each node in row of next_down left to right
+            for right_itr in down_itr.itr_right(excl=True):
+                # unlink top and bottom neighbors of each node reduce count in col header
+                right_itr.up.down = right_itr.down
+                right_itr.down.up = right_itr.up
+                right_itr.get_col().count -= 1
+
+    # uncover a column of a node for dancing links algorithm x
+    @staticmethod
+    def uncover(node: Node) -> None:
+        col = node.get_col()
+        # iterate through each node in column bottom to top
+        for up_itr in col.itr_up(excl=True):
+            # iterate through each node in row of next_up right to left
+            for left_itr in up_itr.itr_left(excl=True):
+                # relink top and bottom neighbors of each node and increment count in col header
+                left_itr.up.down = left_itr
+                left_itr.down.up = left_itr
+                left_itr.get_col().count += 1
+        # relink left and right neighbors of column header
+        col.right.left = col
+        col.left.right = col
+    
+    # search matrix for exact cover, return list of rows as solution if one exists
+    def alg_x_search(self) -> List[Node]:
+        solutions = [] # list of solutions
+        
+        # recursive search algorithm returns true if solution is found
+        def search() -> bool:
+            if self.is_empty(): # if matrix is empty exact cover exists
+                self.solved = True
+                return True
+
+            # select col with least number of 1's
+            selected_col = self.select_min_col()
+            # if selected col has zero 1's, then this branch has failed
+            if selected_col.count < 1 : return False
+
+            # iterate down from col head
+            for down_itr in selected_col.itr_down(excl=True):
+                solutions.append(self.rows[down_itr.row]) # add current row to solutions
+                # iterate right from current node in column and cover each
+                for right_itr in down_itr.itr_right():
+                    if right_itr.col >= 0: DL_Matrix.cover(right_itr)
+
+                # search again after covering
+                # if solution is found on this branch, leave loop and stop searching
+                if search(): break
+
+                # solution not found on this branch, need to uncover columns from this iteration
+                solutions.pop() # remove current row from solutions
+                # iterate left from the last covered column and uncover
+                for left_itr in down_itr.itr_left(excl=True):
+                    if left_itr.col >= 0: DL_Matrix.uncover(left_itr)
+                DL_Matrix.uncover(down_itr )
+            return self.solved
+        
+        if search(): return solutions
+        else: return []
+
+    # inserts Node into Matrix at row, col
+    # if node already exists at row, col - do nothing
     def insert_node(self, row:int, col:int) -> None:
         assert(row >=0 and col >= 0 and row < len(self.rows) and col < len(self.cols))
-
+        # create node to insert
         new_node: Node = Node(self, row, col)
+        # iterate through the row to find correct placement of new_node in row
         for n in self.rows[row].itr_right():
-            if n.right.col == -1 or n.right.col > col:
-                break
-        if n.col == col:
-            return
+            if n.right.col == -1 or n.right.col > col: break
+        if n.col == col: return # if node already exists, leave
+        # reassign left and right pointers
         new_node.right = n.right
         new_node.left = n
         n.right = new_node
         new_node.right.left = new_node
-
+        # iterate through the column to find correct placement of new_node in col
         for n in self.cols[col].itr_down():
-            if n.down.row == -1 or n.down.row > row:
-                break
+            if n.down.row == -1 or n.down.row > row: break
+        # reassign up and down pointers
         new_node.down = n.down
         new_node.up = n
         n.down = new_node
         new_node.down.up = new_node
-
         self.cols[col].count += 1
 
-    # Prints Matrix in tabular form
-    def print_matrix(self):
-        print("\033[4mR| ", end='')
+    # returns True if matrix is empty, False otherwise
+    def is_empty(self) -> bool:
+        return self.root.right == self.root
+
+    # prints Matrix in tabular form
+    def print_matrix(self) -> None:
+        print('n: ', end='')
+        # print counts for each column
+        for col in self.root.itr_right(excl=True):
+            print(col.count, end=' ')
+        print()
+        print(uln + "R| ", end='')
         for col in self.root.itr_right(excl=True):
             print(col.col, end=' ')
-        print("\033[0m")
+        print(res)
         for row in self.root.itr_down(excl=True):
             print(row.row, end='| ')
             col = row
@@ -109,7 +164,8 @@ class DL_Matrix:
 
 # Node to be stored in toroidal linked matrix
 class Node:
-    def __init__(self, matrix:DL_Matrix, row:int, col:int, count:int=-1, up:Node=None, down:Node=None, left:Node=None, right:Node=None) -> None:
+    def __init__(self, matrix:DL_Matrix, row:int, col:int, count:int=-1,
+                 up:Node=None, down:Node=None, left:Node=None, right:Node=None) -> None:
         self.row: int = row
         self.col: int = col
         self.count: int = count
@@ -120,43 +176,44 @@ class Node:
         self.right: Node = right
     
     # iterate full circe from node moving up
-    # If excl is True, this node will not be yielded
-    def itr_up(self, excl=False):
-        start, itr = self, self if not excl else self.up
-        yield itr
+    # if excl is True, this node will not be yielded
+    def itr_up(self, excl=False) -> Generator[Node]:
+        start, itr = self, self
+        if not excl: yield itr
         while itr.up != start:
             itr = itr.up
             yield itr
 
     # iterate full circle from node moving down
-    # If excl is True, this node will not be yielded
-    def itr_down(self, excl=False):
-        start, itr = self, self if not excl else self.down
-        yield itr
+    # if excl is True, this node will not be yielded
+    def itr_down(self, excl=False) -> Generator[Node]:
+        start, itr = self, self
+        if not excl: yield itr
         while itr.down != start:
             itr = itr.down
             yield itr
 
     # iterate full circle from node moving left
-    # If excl is True, this node will not be yielded
-    def itr_left(self, excl=False):
-        start, itr = self, self if not excl else self.left
-        yield itr
+    # if excl is True, this node will not be yielded
+    def itr_left(self, excl=False) -> Generator[Node]:
+        start, itr = self, self
+        if not excl: yield itr
         while itr.left != start:
             itr = itr.left
             yield itr
 
     # iterate full circle from node moving right
-    # If excl is True, this node will not be yielded
-    def itr_right(self, excl=False):
-        start, itr = self, self if not excl else self.right
-        yield itr
+    # if excl is True, this node will not be yielded
+    def itr_right(self, excl=False) -> Generator[Node]:
+        start, itr = self, self
+        if not excl: yield itr
         while itr.right != start:
             itr = itr.right
             yield itr
 
     # return column header of this node
     def get_col(self) -> Node:
+        if self.col == -1: return self.matrix.root
         return self.matrix.cols[self.col]
 
     # returns whether node is in a covered column or not
--- solver.py
+++ solver.py
@@ -1,24 +1,120 @@
+from math import sqrt
 from linked_matrix import *
 
 
-matrix = DL_Matrix(4,4)
+# --- Constraints for a sudoku puzzle ---
+# One value per cell
+# Unique value per row
+# Unique value per col
+# Unique value per box
+# These functions return the column of the matrix to be populated for a constraint when given
+# a specified row of the matrix and the dimension of the sudoku puzzle
+def one_constraint(row:int, dim:int) -> int:
+    return row//dim
+def row_constraint(row:int, dim:int) -> int:
+    return dim**2 + dim*(row//(dim**2)) + row % dim
+def col_constraint(row:int, dim:int) -> int:
+    return 2*(dim**2) + (row % (dim**2))
+def box_constraint(row:int, dim:int) -> int:
+    return int(3*(dim**2) + (row//(sqrt(dim)*dim**2))*(dim*sqrt(dim)) + ((row//(sqrt(dim)*dim)) % sqrt(dim))*dim + (row % dim))
+
+constraints = []
+constraints.append(one_constraint)
+constraints.append(row_constraint)
+constraints.append(col_constraint)
+constraints.append(box_constraint)
+
+# convert list of ints representing the puzzle to a dancing link matrix
+def list_2_matrix(puzzle: List[int]) -> DL_Matrix:
+    dim = int(sqrt(len(puzzle)))
+    num_rows = dim**3
+    num_cols = (dim**2)*len(constraints)
+    matrix: DL_Matrix = DL_Matrix(num_rows, num_cols)
+    #iterate through puzzle
+    for i, cell in enumerate(puzzle):
+        if cell == 0: # if cell is unassigned
+            # populate all rows representing cadidate values for this cell
+            for j in range(dim):
+                row = i*dim+j
+                for constraint in constraints:
+                    matrix.insert_node(row, constraint(row, dim))
+        else: # if cell is assigned
+            # populate the row representing the assigned value for this cell
+            row = i*dim+cell-1
+            for constraint in constraints:
+                    matrix.insert_node(row, constraint(row, dim))
+    return matrix
+
+# decodes the solution list returned by algorithm x
+# returns a list of int representing solution to the puzzle
+def decode_solution(solution_list: List[Node]) -> List[int]:
+    dim = int(sqrt(len(puzzle)))
+    solved_puzzle = [0] * (dim**2)
+    for node in solution_list:
+        solved_puzzle[node.row // dim] = (node.row % dim) + 1
+    return solved_puzzle
+
+# takes list of ints representing a sudoku puzzle
+# returns a list of ints representing the solution if one is found
+def solve_puzzle(puzzle: List[int]) -> List[int]:
+    dim = sqrt(len(puzzle))
+    assert(int(dim+0.5)**2 == len(puzzle)) # only perfect square puzzles are supported
+    puzzle_matrix = list_2_matrix(puzzle)
+    solution_list = puzzle_matrix.alg_x_search()
+    if solution_list: return decode_solution(solution_list)
+    else: return puzzle
+
+# prints a list of ints as a sudoku puzzle
+def print_puzzle(puzzle: List[int]) -> None:
+    dim = sqrt(len(puzzle))
+    assert(int(dim+0.5)**2 == len(puzzle)) # only perfect square puzzles are supported
+    dim = int(dim)
+    for i in range(dim): print(uln + '   ', end='')
+    print('     ' + res, end='')
+    for i, cell in enumerate(puzzle):
+        if i % int(sqrt(dim)) == 0 and i != 0: print("|", end='')
+        if i//dim % int(sqrt(dim)) == int(sqrt(dim)) -1: print(uln + "", end='')
+        if i % dim == 0: print('\n|', end='')
+        print(f"{cell:3d}", end='')
+        print(res, end='')
+    print("|")
 
 
-# for n in matrix.rows[2].itr_up():
-#     print(n.row)
-# for n in matrix.root.itr_down():
-#     print(n.row)
-# for n in matrix.root.itr_right():
-#     print(n.col)
-# for n in matrix.root.itr_left():
-#     print(n.col)
+if __name__ == '__main__':
+    import time
 
-matrix.insert_node(1,2)
-matrix.insert_node(0,1)
-matrix.insert_node(0,0)
-matrix.insert_node(2,0)
-matrix.insert_node(3,1)
-matrix.insert_node(3,3)
-
-matrix.print_matrix()
-print(matrix.select_min_col().col)
+    puzzle = \
+    [15, 0, 0,13, 0,14,11, 8, 3, 0, 0, 1, 4, 0, 0, 7,
+      3, 8, 0, 0,15, 2, 6, 0, 0, 0, 0, 0, 1, 0,14, 0,
+      6, 0, 1, 0,10, 7, 0,12,15, 0, 5, 0, 2, 0,11, 3,
+     11, 0, 0, 0, 0, 3, 0, 0, 4, 0,13,14, 0, 0, 0, 6,
+      8, 3,10,15, 0,16,13, 0, 7, 0, 0, 6,14, 5, 0,12,
+      0, 0, 0, 1, 0, 0,14, 0,13, 0, 0,15, 6, 7, 0, 0,
+      7, 2,12, 6, 8, 4, 0,10, 5, 9, 0,16, 0,11, 1,15,
+      0,13,14, 0, 7, 6, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
+      0, 5,11, 0,14,13, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0,
+      1, 7, 2,10,12, 8, 0, 4, 6, 3, 0, 5, 0,14, 9,13,
+      0, 0, 0, 3, 0, 0,10, 0, 9, 0, 0, 4,12,16, 0, 0,
+     14, 4, 9, 8, 0,15, 2, 0,12, 0, 0,11, 7, 6, 0,10,
+     12, 0, 0, 0, 0,10, 0, 0,16, 0, 9,13, 0, 0, 0, 2,
+     10, 0,13, 0, 4, 1, 0, 2,14, 0, 6, 0, 9, 0, 8,11,
+      2,15, 0, 0, 6, 9, 7, 0, 0, 0, 0, 0,10, 0,13, 0,
+      9, 0, 0,14, 0,12, 8, 3, 1, 0, 0, 2, 5, 0, 0,16]
+    # puzzle = \
+    # [0,9,0,2,0,0,0,0,0,
+    #  8,2,3,0,0,5,0,0,0,
+    #  0,4,0,0,3,0,0,0,0,
+    #  3,0,0,6,0,0,0,0,8,
+    #  4,0,0,0,7,0,0,0,6,
+    #  7,0,0,0,0,9,0,0,4,
+    #  0,0,0,0,2,0,0,5,0,
+    #  0,0,0,1,0,0,7,8,3,
+    #  0,0,0,0,0,8,0,1,0]
+    
+    start = time.process_time()
+    solution = solve_puzzle(puzzle)
+    total_time = time.process_time() - start
+    
+    print_puzzle(puzzle)
+    print(f"\nSolution Found In: {total_time} seconds")
+    print_puzzle(solution)

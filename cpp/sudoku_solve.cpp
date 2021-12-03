4fda3a5f94113549c517b2341ede240dcc77bd8a
sernik <nickstagman@gmail.com>
2021-12-03 01:27:03 -0500

    refactored cpp implementation

--- cpp/sudoku_solve.cpp
+++ cpp/sudoku_solve.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <fstream>
-// #include <string>
 #include "dlinks_matrix.h"
 
 
@@ -23,29 +21,38 @@ bool solve_puzzle(Matrix* matrix, int* puzzle, int* solution){
     return found;
 }
 
-
-int main(){
-    Matrix solver;
-    char buf[1024];
+//accepts up to 2 arguments: 
+//  1 - file of puzzles, puzzles are a new-line delimited string of numbers with 0 representing empty boxes
+//      defaults to "puzzles.txt"
+//  2 - output file, solution will be written to the output file in the same format as the input file
+//      defaults to "solutions.txt"
+int main(int argc, char *argv[]){
+    Matrix matrix;
     int solution[81];
     int puzzle[81];
+    char buf[82];
     char pstr[81];
+    std::FILE* puzzle_file;
+    std::FILE* solutions_file;
 
-    std::fstream fs;
-    fs.open("puzzle.txt", std::fstream::in | std::fstream::out);
-    std::FILE* puzzle_file = std::fopen("puzzles.txt", "r+");
-    while(std::fgets(buf, 1024, puzzle_file) != 0){
+    if(argc > 1) { puzzle_file = std::fopen(argv[1], "r"); }
+    else { puzzle_file = std::fopen("puzzles.txt", "r"); }
+    if(argc > 2) { solutions_file = std::fopen(argv[2], "w"); }
+    else { solutions_file = std::fopen("solutions.txt", "w"); }
+
+    while(std::fgets(buf, 83, puzzle_file) != 0){
+        buf[81] = '\0';
         for(int i=0; i<81; i++){
             puzzle[i] = buf[i] - '0';
         }
-        solve_puzzle(&solver, puzzle, solution);
+        solve_puzzle(&matrix, puzzle, solution);
         for(int i=0; i<81; i++){
             pstr[i] = solution[i] + '0';
         }
-        std::fputs(" ", puzzle_file);
-        std::fputs(pstr, puzzle_file);
-        std::fputs("\n", puzzle_file);
-        
+        std::fputs(buf, solutions_file);
+        std::fputs(" ", solutions_file);
+        std::fputs(pstr, solutions_file);
+        std::fputs("\n", solutions_file);
     }
     return 0;
 }
